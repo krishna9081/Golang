0624ba0e9f865a0c2628576781be145fab2a6d25
Krishna Rachakonda <krishna9081@gmail.com>
2022-08-17 12:01:07 -0500

    longestsubstring

---
+++ leetcode/longestsubstring.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+
+	var start, end, maxLen int
+	m := make(map[byte]int)
+
+	//moving the end pointer
+	for end < len(s) {
+
+		// If a repeating character is found stop and update start pointer
+		if _, ok := m[s[end]]; ok {
+			start = max(start, m[s[end]]+1)
+		}
+		//if not found , update map
+		m[s[end]] = end
+		maxLen = max(maxLen, end-start+1)
+		end++
+	}
+	return maxLen
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func main() {
+
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+
+}
