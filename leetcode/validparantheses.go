2befb6642af834e4b5fb3262e94ade14d6c3e779
Krishna Rachakonda <krishna9081@gmail.com>
2022-08-17 22:56:37 -0500

    valid parantheses-more comments

--- leetcode/validparantheses.go
+++ leetcode/validparantheses.go
@@ -7,18 +7,22 @@ func minRemoveToMakeValid(s string) string {
 	stack := []int{}
 	//Iterating over slice
 	for i := 0; i < len(s); i++ {
-		// String check ...Should be single quotes
+		// String check ...Should be single quotes ..Keeping the index of opening braces in the stack
 		if s[i] == '(' {
 			//appened method is different from Python
 			stack = append(stack, i)
-		} else if s[i] == ')' && len(stack) != 0 {
+		} else if s[i] == ')' && len(stack) != 0 { //unwinding the stack as long as you see matching open braces until the length of the stack
+			//Bottom line ...never add index of closing braces to the stack
 			stack = stack[:len(stack)-1]
-		} else if s[i] == ')' {
+		} else if s[i] == ')' { // If the length of stack is already empty ...if you still find a closing brace from the stack
+			// Just make the element disappear from the stack Bottom line ...never add index of closing braces to the stack
 			s = s[:i] + s[i+1:]
 			i--
 		}
 	}
 
+	// You may still find opening brace indices left out in the stack ... capture those one by one , remove those from the actual string
+	//Resultant string is the one which has matching parentheses with minimum number of removals
 	for len(stack) != 0 {
 		lastIdx := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
