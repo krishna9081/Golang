343cb0ed69968677b8092479322484cd925c089f
Krishna Rachakonda <krishna9081@gmail.com>
2022-09-05 22:39:06 -0500

    Sum root to leaf numbers-Comments

--- leetcode/sumroottoleafnumbers.go
+++ leetcode/sumroottoleafnumbers.go
@@ -17,16 +17,16 @@ func sumNumbers(root *TreeNode) int {
 }
 
 func dfsHelper(node *TreeNode, sum int) int {
-	//Leaf node
+	//Leaf node - All that leaf node is doing is adding Value to the actual sum
 	if node.Left == nil && node.Right == nil {
 		return node.Val + sum
-	} else if node.Left == nil {
+	} else if node.Left == nil { // recursive cases value is added to sum and the sum is multiplied by 10
 		sum += node.Val
 		return dfsHelper(node.Right, sum*10)
 	} else if node.Right == nil {
 		sum += node.Val
 		return dfsHelper(node.Left, sum*10)
-	} else {
+	} else { //special recursive case when both the children exist ...sums of both the children get added
 		sum += node.Val
 		return dfsHelper(node.Left, sum*10) + dfsHelper(node.Right, sum*10)
 	}
